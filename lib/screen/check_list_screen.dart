274fc4d3809f163702850ed2240116fd970e3bf9
pyo_16 <94816219+pyowonsik@users.noreply.github.com>
2023-03-16 10:52:27 +0900

    rename todo

--- lib/screen/check_list_screen.dart
+++ lib/screen/check_list_screen.dart
@@ -1,4 +1,4 @@
-import 'package:card_list/todo/card_model.dart';
+import 'package:card_list/card/card_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
